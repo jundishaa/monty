17fecd9adf6b36c461e5f014347537936d9f3d12
jundi yusuf <jundiyusuf10@gmail.com>
2024-03-28 15:05:04 -0700

    swap

--- swap.c
+++ swap.c
@@ -8,15 +8,30 @@
 void swap(stack_t **stack, unsigned int line_number)
 {
 	int temp;
+	stack_t *first, *second;
 
-    /* Check if stack contains less than two elements */
-	if (!stack || !*stack || !(*stack)->next)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	temp = (*stack)->n;
-	(*stack)->n = (*stack)->next->n;
-	(*stack)->next->n = temp;
-}
 
+	first = *stack;
+	second = (*stack)->next;
+
+    /* Swap values */
+	temp = first->n;
+	first->n = second->n;
+	second->n = temp;
+
+    /* Swap next pointers (if not top two elements) */
+	if (second->next)
+	{
+		second->next->prev = first;
+	}
+	first->next = second->next;
+	second->next = first;
+
+    /* Update stack head */
+	*stack = second;
+}
